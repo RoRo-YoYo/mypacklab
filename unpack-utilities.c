241d37a432851ab5623dd423a2abdd8a0d791cf9
RoRo-YoYo <RocelynYoung2027@u.northwestern.edu>
2026-01-15 10:03:53 -0600

    skeleton foe calculate sum

--- unpack-utilities.c
+++ unpack-utilities.c
@@ -71,46 +71,45 @@ void parse_header(uint8_t* input_data, size_t input_len, packlab_config_t* confi
   }  
   else {
     config->is_valid = false;
-    printf("Valid input data length:",input_len);
+    printf("Invalid input data length:",input_len);
   }
 
   //  Check which options are set in Flags, set the appropriate fields in the struct, and determine how
   //  many more bytes need to be read from the header.
-  config->is_compressed = input_data[3] >> 7; // Take the MSB (7 bit)
-  config->is_encrypted = (input_data[3] ^ 191) >> 6; // Mask and take the 6 bit
-  config->is_checksummed = (input_data[3] ^ 223) >> 5; // Mask and take the 5 bit
-  config->should_continue = (input_data[3] ^ 239) >> 4; // Mask and take the 4 bit
-  config->should_float = (input_data[3] ^ 247) >> 3; // Mask and take the 3 bit
-  config->should_float3 = (input_data[3] ^ 251) >> 2; // Mask and take the 3 bit
-  
-  //   Get the length of this stream and the length of the original data.
-  config->orig_data_size = ((uint64_t)input_data[11] << 56) + ((uint64_t)input_data[10] << 48) + ((uint64_t)input_data[9] << 40) + ((uint64_t)input_data[8] << 32) + ((uint64_t)input_data[7] << 24)+ ((uint64_t)input_data[6] << 16) + ((uint64_t)input_data[5] << 8) + ((uint64_t)input_data[4]); // Cast into 64-bit to avoid undefine when shifting; Left shift to create trailing zero and add up
-  config->data_size = ((uint64_t)input_data[19] << 56) + ((uint64_t)input_data[18] << 48) + ((uint64_t)input_data[17] << 40) + ((uint64_t)input_data[16] << 32) + ((uint64_t)input_data[15] << 24)+ ((uint64_t)input_data[14] << 16) + ((uint64_t)input_data[13] << 8) + ((uint64_t)input_data[12]); // Cast into 64-bit to avoid undefine when shifting; Left shift to create trailing zero and add up
-  
-  // Pull out the compression dictionary for this stream if Compression? is enabled.
-  if (config->is_compressed == true ) { // Put value in Address 20, 24, 28, 32 (20-35)
-    for (int i = 0; i < 16 ; ++i) { // Put values by looping
-      input_data[20+i] = config->dictionary_data[0+i];
-    }
-    config->header_len = 36; // Update header len with compression condition; (Address 0 - 35)
+  if (config->is_valid == true) {
+    config->is_compressed = input_data[3] >> 7; // Take the MSB (7 bit)
+    config->is_encrypted = (input_data[3] ^ 191) >> 6; // Mask and take the 6 bit
+    config->is_checksummed = (input_data[3] ^ 223) >> 5; // Mask and take the 5 bit
+    config->should_continue = (input_data[3] ^ 239) >> 4; // Mask and take the 4 bit
+    config->should_float = (input_data[3] ^ 247) >> 3; // Mask and take the 3 bit
+    config->should_float3 = (input_data[3] ^ 251) >> 2; // Mask and take the 3 bit
+    
+    //   Get the length of this stream and the length of the original data.
+    config->orig_data_size = ((uint64_t)input_data[11] << 56) + ((uint64_t)input_data[10] << 48) + ((uint64_t)input_data[9] << 40) + ((uint64_t)input_data[8] << 32) + ((uint64_t)input_data[7] << 24)+ ((uint64_t)input_data[6] << 16) + ((uint64_t)input_data[5] << 8) + ((uint64_t)input_data[4]); // Cast into 64-bit to avoid undefine when shifting; Left shift to create trailing zero and add up
+    config->data_size = ((uint64_t)input_data[19] << 56) + ((uint64_t)input_data[18] << 48) + ((uint64_t)input_data[17] << 40) + ((uint64_t)input_data[16] << 32) + ((uint64_t)input_data[15] << 24)+ ((uint64_t)input_data[14] << 16) + ((uint64_t)input_data[13] << 8) + ((uint64_t)input_data[12]); // Cast into 64-bit to avoid undefine when shifting; Left shift to create trailing zero and add up
+    
+    // Pull out the compression dictionary for this stream if Compression? is enabled.
+    if (config->is_compressed == true ) { // Put value in Address 20, 24, 28, 32 (20-35)
+      for (int i = 0; i < 16 ; ++i) { // Put values by looping
+        input_data[20+i] = config->dictionary_data[0+i];
+      }
+      config->header_len = 36; // Update header len with compression condition; (Address 0 - 35)
 
-    // Check if checksum is also enabled; 16-bits separated into two bytes (8-bits)
+      // Check if checksum is also enabled; 16-bits separated into two bytes (8-bits)
+      if (config->is_checksummed == true ) {
+        input_data[36] = (uint8_t)(config->checksum_value >> 8);  // big-endian, right to left; Take the first two byte first; Shift to the right, leaving leading zero, which is dropped when cast into 8-bit 
+        input_data[37] = (uint8_t)(config->checksum_value ^ 65280); // XOR by 1111111100000000 to clear the first two bytes, leaving the last two bytes. Cast ito 8-bit
+        config->header_len = 38; // Update header len with compression and checksum condition; (Address 0 - 37)
+      }
+    }
+
+    // Check if checksum is enabled alone; Pull out the checksum value for this stream if Checksummed? is enabled.
     if (config->is_checksummed == true ) {
-      input_data[36] = (uint8_t)(config->checksum_value >> 8);  // big-endian, right to left; Take the first two byte first; Shift to the right, leaving leading zero, which is dropped when cast into 8-bit 
-      input_data[37] = (uint8_t)(config->checksum_value ^ 65280); // XOR by 1111111100000000 to clear the first two bytes, leaving the last two bytes. Cast ito 8-bit
-      config->header_len = 38; // Update header len with compression and checksum condition; (Address 0 - 37)
+      input_data[20] = (uint8_t)(config->checksum_value >> 8);  // big-endian, right to left; Take the first two byte first; Shift to the right, leaving leading zero, which is dropped when cast into 8-bit 
+      input_data[21] = (uint8_t)(config->checksum_value ^ 65280); // XOR by 1111111100000000 to clear the first two bytes, leaving the last two bytes. Cast ito 8-bit
+      config->header_len = 22; // Update header len with checksum condition; (Address 0 - 22)
     }
   }
-
-  // Check if checksum is enabled alone; Pull out the checksum value for this stream if Checksummed? is enabled.
-  if (config->is_checksummed == true ) {
-    input_data[20] = (uint8_t)(config->checksum_value >> 8);  // big-endian, right to left; Take the first two byte first; Shift to the right, leaving leading zero, which is dropped when cast into 8-bit 
-    input_data[21] = (uint8_t)(config->checksum_value ^ 65280); // XOR by 1111111100000000 to clear the first two bytes, leaving the last two bytes. Cast ito 8-bit
-    config->header_len = 22; // Update header len with checksum condition; (Address 0 - 22)
-  }
-
-  // Finding length of Header 
-
 }
 
 uint16_t calculate_checksum(uint8_t* input_data, size_t input_len) {
@@ -118,6 +117,15 @@ uint16_t calculate_checksum(uint8_t* input_data, size_t input_len) {
   // TODO
   // Calculate a checksum over input_data
   // Return the checksum value
+
+  // Figure out configuration
+  // If compression and 
+  if ((input_data[3] >> 7 == 1) & input_data[3] ^ 191) {
+    
+  }
+  else {
+    return 0;
+  }
 
   return 0;
 }
